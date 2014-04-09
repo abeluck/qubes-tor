1b243a88f93637f8ad2a027ea1dfb128c9d2651a
Abel Luck <abel@guardianproject.info>
2014-04-09 19:52:25 +0200

    kernel bug workaround to prevent leaking packets
    Add iptables rules to workaround apparent kernel bug that leaks
    FIN packets. Props to Mike Perry.
    
    https://lists.torproject.org/pipermail/tor-talk/2014-March/032507.html

--- start_tor_proxy.sh
+++ start_tor_proxy.sh
@@ -73,6 +73,17 @@ function setup_firewall
 	/sbin/iptables -A INPUT -i lo -j ACCEPT
 	/sbin/iptables -A INPUT -j REJECT --reject-with icmp-host-prohibited
 
+    # prevent kernel bug transproxy leak
+    # https://lists.torproject.org/pipermail/tor-talk/2014-March/032507.html
+    #/sbin/iptables -A OUTPUT -m conntrack --ctstate INVALID -j LOG --log-prefix "Transproxy ctstate leak blocked: " --log-uid
+    /sbin/iptables -A OUTPUT -m conntrack --ctstate INVALID -j DROP
+    #/sbin/iptables -A OUTPUT -m state --state INVALID -j LOG --log-prefix "Transproxy state leak blocked: " --log-uid
+    /sbin/iptables -A OUTPUT -m state --state INVALID -j DROP
+    #/sbin/iptables -A OUTPUT ! -o lo ! -d 127.0.0.1 ! -s 127.0.0.1 -p tcp -m tcp --tcp-flags ACK,FIN ACK,FIN -j LOG --log-prefix "Transproxy leak blocked: " --log-uid
+    #/sbin/iptables -A OUTPUT ! -o lo ! -d 127.0.0.1 ! -s 127.0.0.1 -p tcp -m tcp --tcp-flags ACK,RST ACK,RST -j LOG --log-prefix "Transproxy leak blocked: " --log-uid
+    /sbin/iptables -A OUTPUT ! -o lo ! -d 127.0.0.1 ! -s 127.0.0.1 -p tcp -m tcp --tcp-flags ACK,FIN ACK,FIN -j DROP
+    /sbin/iptables -A OUTPUT ! -o lo ! -d 127.0.0.1 ! -s 127.0.0.1 -p tcp -m tcp --tcp-flags ACK,RST ACK,RST -j DROP
+
 	# nat rules
 	/sbin/iptables -t nat -F
 	/sbin/iptables -t nat -P PREROUTING ACCEPT
