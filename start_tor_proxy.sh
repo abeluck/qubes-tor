8183a8363ac6c54b8202732026f00ec253e32fcc
Abel Luck <abel@guardianproject.info>
2014-04-09 19:58:01 +0200

    convert tabs to spaces

--- start_tor_proxy.sh
+++ start_tor_proxy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-# 
+#
 # The Qubes OS Project, http://www.qubes-os.org
 #
-# Copyright (C) 2012-2013 Abel Luck <abel@outcomedubious.im>
+# Copyright (C) 2012-2014 Abel Luck <abel@outcomedubious.im>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -46,32 +46,32 @@ PID=/var/run/qubes-tor.pid
 # $2 = template file
 function replace_vars()
 {
-	for var in $1; do
-		expressions+=("-e s|$var|${!var}|g")
-	done
+    for var in $1; do
+        expressions+=("-e s|$var|${!var}|g")
+    done
 
-	sed "${expressions[@]}" $2
+    sed "${expressions[@]}" $2
 }
 
 function setup_firewall
 {
 
-	echo "0" > /proc/sys/net/ipv4/ip_forward
-	/sbin/iptables -F
-	/sbin/iptables -P INPUT DROP
-	/sbin/iptables -P FORWARD ACCEPT
-	/sbin/iptables -P OUTPUT ACCEPT
-	/sbin/iptables -A INPUT -i vif+ -p udp -m udp --dport 53 -j ACCEPT
-	/sbin/iptables -A INPUT -i vif+ -p tcp -m tcp --dport $TOR_TRANS_PORT -j ACCEPT
-	/sbin/iptables -A INPUT -i vif+ -p tcp -m tcp --dport $TOR_SOCKS_PORT -j ACCEPT
-	/sbin/iptables -A INPUT -i vif+ -p tcp -m tcp --dport $TOR_SOCKS_ISOLATED_PORT -j ACCEPT
-	if [ "$TOR_CONTROL_PORT" != "0" ]; then
-		/sbin/iptables -A INPUT -i vif+ -p tcp -m tcp --dport $TOR_CONTROL_PORT -j ACCEPT
-	fi
-	/sbin/iptables -A INPUT -i vif+ -p udp -m udp -j DROP
-	/sbin/iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-	/sbin/iptables -A INPUT -i lo -j ACCEPT
-	/sbin/iptables -A INPUT -j REJECT --reject-with icmp-host-prohibited
+    echo "0" > /proc/sys/net/ipv4/ip_forward
+    /sbin/iptables -F
+    /sbin/iptables -P INPUT DROP
+    /sbin/iptables -P FORWARD ACCEPT
+    /sbin/iptables -P OUTPUT ACCEPT
+    /sbin/iptables -A INPUT -i vif+ -p udp -m udp --dport 53 -j ACCEPT
+    /sbin/iptables -A INPUT -i vif+ -p tcp -m tcp --dport $TOR_TRANS_PORT -j ACCEPT
+    /sbin/iptables -A INPUT -i vif+ -p tcp -m tcp --dport $TOR_SOCKS_PORT -j ACCEPT
+    /sbin/iptables -A INPUT -i vif+ -p tcp -m tcp --dport $TOR_SOCKS_ISOLATED_PORT -j ACCEPT
+    if [ "$TOR_CONTROL_PORT" != "0" ]; then
+        /sbin/iptables -A INPUT -i vif+ -p tcp -m tcp --dport $TOR_CONTROL_PORT -j ACCEPT
+    fi
+    /sbin/iptables -A INPUT -i vif+ -p udp -m udp -j DROP
+    /sbin/iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+    /sbin/iptables -A INPUT -i lo -j ACCEPT
+    /sbin/iptables -A INPUT -j REJECT --reject-with icmp-host-prohibited
 
     # prevent kernel bug transproxy leak
     # https://lists.torproject.org/pipermail/tor-talk/2014-March/032507.html
@@ -84,47 +84,47 @@ function setup_firewall
     /sbin/iptables -A OUTPUT ! -o lo ! -d 127.0.0.1 ! -s 127.0.0.1 -p tcp -m tcp --tcp-flags ACK,FIN ACK,FIN -j DROP
     /sbin/iptables -A OUTPUT ! -o lo ! -d 127.0.0.1 ! -s 127.0.0.1 -p tcp -m tcp --tcp-flags ACK,RST ACK,RST -j DROP
 
-	# nat rules
-	/sbin/iptables -t nat -F
-	/sbin/iptables -t nat -P PREROUTING ACCEPT
-	/sbin/iptables -t nat -P INPUT ACCEPT
-	/sbin/iptables -t nat -P OUTPUT ACCEPT
-	/sbin/iptables -t nat -P POSTROUTING ACCEPT
-	/sbin/iptables -t nat -A PREROUTING -i vif+ -p udp -m udp --dport 53 -j DNAT --to-destination $QUBES_IP:53
-	/sbin/iptables -t nat -A PREROUTING -i vif+ -p tcp -m tcp --dport $TOR_SOCKS_ISOLATED_PORT -j DNAT --to-destination $QUBES_IP:$TOR_SOCKS_ISOLATED_PORT
-	/sbin/iptables -t nat -A PREROUTING -i vif+ -p tcp -m tcp --dport $TOR_SOCKS_PORT -j DNAT --to-destination $QUBES_IP:$TOR_SOCKS_PORT
-	/sbin/iptables -t nat -A PREROUTING -i vif+ -p tcp -j DNAT --to-destination $QUBES_IP:$TOR_TRANS_PORT
-	echo "1" > /proc/sys/net/ipv4/ip_forward
+    # nat rules
+    /sbin/iptables -t nat -F
+    /sbin/iptables -t nat -P PREROUTING ACCEPT
+    /sbin/iptables -t nat -P INPUT ACCEPT
+    /sbin/iptables -t nat -P OUTPUT ACCEPT
+    /sbin/iptables -t nat -P POSTROUTING ACCEPT
+    /sbin/iptables -t nat -A PREROUTING -i vif+ -p udp -m udp --dport 53 -j DNAT --to-destination $QUBES_IP:53
+    /sbin/iptables -t nat -A PREROUTING -i vif+ -p tcp -m tcp --dport $TOR_SOCKS_ISOLATED_PORT -j DNAT --to-destination $QUBES_IP:$TOR_SOCKS_ISOLATED_PORT
+    /sbin/iptables -t nat -A PREROUTING -i vif+ -p tcp -m tcp --dport $TOR_SOCKS_PORT -j DNAT --to-destination $QUBES_IP:$TOR_SOCKS_PORT
+    /sbin/iptables -t nat -A PREROUTING -i vif+ -p tcp -j DNAT --to-destination $QUBES_IP:$TOR_TRANS_PORT
+    echo "1" > /proc/sys/net/ipv4/ip_forward
 
-	# completely disable ipv6
-	/sbin/ip6tables -P INPUT DROP
-	/sbin/ip6tables -P OUTPUT DROP
-	/sbin/ip6tables -P FORWARD DROP
-	/sbin/ip6tables -F
+    # completely disable ipv6
+    /sbin/ip6tables -P INPUT DROP
+    /sbin/ip6tables -P OUTPUT DROP
+    /sbin/ip6tables -P FORWARD DROP
+    /sbin/ip6tables -F
 
-	for iface in `ls /proc/sys/net/ipv6/conf/vif*/disable_ipv6 2> /dev/null`; do
-		echo "1" > $iface
-	done
+    for iface in `ls /proc/sys/net/ipv6/conf/vif*/disable_ipv6 2> /dev/null`; do
+        echo "1" > $iface
+    done
 }
 
 # function to print error and setup firewall rules to prevent traffic leaks
 function exit_error()
 {
-	echo "qubes-tor: $1" 1>&2
-	setup_firewall
-	exit 1
+    echo "qubes-tor: $1" 1>&2
+    setup_firewall
+    exit 1
 }
 
 # double check we've got an ip address
 if [ X$QUBES_IP == X ]; then
-	QUBES_IP="127.0.0.1"
-	exit_error "Error getting qubes ip"
+    QUBES_IP="127.0.0.1"
+    exit_error "Error getting qubes ip"
 fi
 
 
 # make the data directory if it doesn't exist
 if [ ! -d "$DATA_DIRECTORY" ]; then
-	mkdir -p $DATA_DIRECTORY || exit_error "Error creating data directory"
+    mkdir -p $DATA_DIRECTORY || exit_error "Error creating data directory"
 fi
 
 # pass the -f option only when config file exists
@@ -137,18 +137,18 @@ fi
 
 # verify config file is useable
 /usr/bin/tor \
-	--defaults-torrc $DEFAULT_RC \
-	$USER_RC_OPTION --verify-config \
-|| exit_error "Error in Tor configuration"
+    --defaults-torrc $DEFAULT_RC \
+    $USER_RC_OPTION --verify-config \
+    || exit_error "Error in Tor configuration"
 
 # start tor
 /usr/bin/tor \
-	--defaults-torrc $DEFAULT_RC \
-	$USER_RC_OPTION \
-	--RunAsDaemon 1 \
-	--Log "notice syslog" \
-	--PIDFile $PID \
-|| exit_error "Error starting Tor!"
+    --defaults-torrc $DEFAULT_RC \
+    $USER_RC_OPTION \
+    --RunAsDaemon 1 \
+    --Log "notice syslog" \
+    --PIDFile $PID \
+    || exit_error "Error starting Tor!"
 
 # if we get here tor is running
 setup_firewall
